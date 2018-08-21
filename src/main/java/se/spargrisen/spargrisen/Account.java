da19734315a6f8a690ab532e09733411f7aca82a
Administrator <emmaljungdahl@gmail.com>
2018-08-21 14:50:44 +0200

    Accountclass with withdraw method

---
+++ src/main/java/se/spargrisen/spargrisen/Account.java
@@ -0,0 +1,29 @@
+package se.spargrisen.spargrisen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+
+    private String name;
+    private double balance;
+    public List<Integer> transactions = new ArrayList<>();
+
+    public Account(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) {
+
+        if (balance >= amount)
+            balance -= amount;
+        else
+            System.out.println("Balance is too low.");
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
