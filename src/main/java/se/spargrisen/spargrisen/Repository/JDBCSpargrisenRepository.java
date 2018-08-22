be2fa4fce9c709cb6440da364bcd48fd7b92fe4c
Administrator <emmaljungdahl@gmail.com>
2018-08-22 19:35:59 +0200

    Started on JBDCSpargrisenRepository

--- src/main/java/se/spargrisen/spargrisen/Repository/JDBCSpargrisenRepository.java
+++ src/main/java/se/spargrisen/spargrisen/Repository/JDBCSpargrisenRepository.java
@@ -1,28 +1,36 @@
 package se.spargrisen.spargrisen.Repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.Map;
+
 
 @Component
 public class JDBCSpargrisenRepository implements SpargrisenRepository {
 
-//    @Override
-//    public List<??> ??() {
-//        try (Connection conn = ???.getConnection();
-//             Statement stmt = conn.createStatement();
-//             ResultSet rs = stmt.executeQuery("SELECT id, title FROM blogs")) {
-//            List<Blog> blogs = new ArrayList<>();
-//            while (rs.next()) blogs.add(rsBlog(rs));
-//            return blogs;
-//        } catch (SQLException e) {
-//            throw new BlogRepositoryException(e);
-//        }
-//    }
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public Map<Integer, String> getCategories() {
+        try (Connection conn = dataSource.getConnection();
+            Statement statement = conn.createStatement();
+             ResultSet rs = statement.executeQuery("SELECT c.category_ID, c.name " +
+                     " FROM categories c " +
+                     " INNER JOIN "
+                     " WHERE c.category_ID = ") {
+
+             } catch (SQLException e) {
+
+        }
+
+    }
 
 }
 
