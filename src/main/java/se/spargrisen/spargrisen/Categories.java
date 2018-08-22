f68bf4a8a62e3b4ee9f62abd2ca5ade3f5e0b410
Katrin Eckert <katrin.82.eckert@gmail.com>
2018-08-22 14:39:36 +0200

    Add dropdown and connected enum

--- src/main/java/se/spargrisen/spargrisen/Categories.java
+++ src/main/java/se/spargrisen/spargrisen/Categories.java
@@ -1,20 +1,36 @@
 package se.spargrisen.spargrisen;
 
 public enum Categories {
-    BOENDE,
-    LIVSMEDEL,
-    NÖJEN,
-    AVGIFTER,
-    TELEFONI,
-    RESTURANGBESÖK,
-    SHOPPING,
-    KLÄDER,
-    HUSDJUR,
-    FÖRSÄKRINGAR,
-    TRANSPORT,
-    SPARANDE,
-    LÅN,
-    TRÄNING,
-    HYGIEN,
-    HUSHÅLLSARTIKLAR
+
+    BOENDE ("Boende"),
+    LIVSMEDEL ("Livsmedel"),
+    NÖJEN ("Nöjen"),
+    AVGIFTER ("Avgifter"),
+    TELEFONI ("Telefoni"),
+    RESTURANGBESÖK ("Restaurangbesök"),
+    SHOPPING ("Shopping"),
+    KLÄDER ("Kläder"),
+    HUSDJUR ("Husdjur"),
+    FÖRSÄKRINGAR ("Försäkringar"),
+    TRANSPORT ("Transport"),
+    SPARANDE ("Sparande"),
+    LÅN ("Lån"),
+    TRÄNING ("Träning"),
+    HYGIEN ("Hygien"),
+    HUSHÅLLSARTIKLAR ("Hushållsartiklar");
+
+    private final String value;
+
+    private Categories(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString (){
+        return value;
+    }
 }
