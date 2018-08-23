c7e23308cf01c110ece5cc8ef1e8e77578cef38b
Administrator <emmaljungdahl@gmail.com>
2018-08-23 11:15:12 +0200

    Added SQL statement for categories

--- src/main/java/se/spargrisen/spargrisen/Repository/JDBCSpargrisenRepository.java
+++ src/main/java/se/spargrisen/spargrisen/Repository/JDBCSpargrisenRepository.java
@@ -2,10 +2,13 @@ package se.spargrisen.spargrisen.Repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import se.spargrisen.spargrisen.Category;
 
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -33,7 +36,7 @@ public class JDBCSpargrisenRepository implements SpargrisenRepository {
     public void deposit(double income, int id) {
         try (Connection conn = dataSource.getConnection();
              Statement stmt = conn.createStatement();
-             PreparedStatement ps = conn.prepareStatement("SELECT balance + income FROM accounts where account_id= ?")){
+             PreparedStatement ps = conn.prepareStatement("SELECT balance + income FROM accounts where account_id= ?")) {
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
 
@@ -44,18 +47,49 @@ public class JDBCSpargrisenRepository implements SpargrisenRepository {
 
 
     @Override
-    public Map<Integer, String> getCategories() {
+    public List<Category> getCategories(int userId) {
         try (Connection conn = dataSource.getConnection();
-            Statement statement = conn.createStatement();
-             ResultSet rs = statement.executeQuery("SELECT c.category_ID, c.name " +
+             Statement statement = conn.createStatement();
+             PreparedStatement ps = conn.prepareStatement("SELECT c.category_ID, c.name " +
                      " FROM categories c " +
                      " INNER JOIN users_categories AS uc " +
                      " ON c.category_ID = uc.category_ID" +
-                     " WHERE uc.user_ID = ?");
+                     " WHERE uc.user_ID = ?")) {
+            ps.setInt(1, userId);
+            ResultSet rs = ps.executeQuery();
+            List<Category> categories = new ArrayList<>();
+            while (rs.next()) categories.add(rsCategory(rs));
+            return categories;
 
-             } catch (SQLException e) {
-
+        } catch (SQLException e) {
+            throw new SpargrisenRepositoryExeption(e);
         }
+
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        try (Connection conn = dataSource.getConnection();
+             Statement statement = conn.createStatement();
+             PreparedStatement ps = conn.prepareStatement("SELECT c.category_ID, c.name " +
+                     " FROM categories c "
+             )) {
+            ResultSet rs = ps.executeQuery();
+            List<Category> categories = new ArrayList<>();
+            while (rs.next()) categories.add(rsCategory(rs));
+            return categories;
+
+        } catch (SQLException e) {
+            throw new SpargrisenRepositoryExeption(e);
+        }
+
+    }
+
+    private Category rsCategory(ResultSet rs) throws SQLException {
+        return new Category(
+                rs.getInt("category_id"),
+                rs.getString("name")
+        );
 
     }
 
--- src/main/java/se/spargrisen/spargrisen/Repository/SpargrisenRepository.java
+++ src/main/java/se/spargrisen/spargrisen/Repository/SpargrisenRepository.java
@@ -1,9 +1,14 @@
 package se.spargrisen.spargrisen.Repository;
 
+import se.spargrisen.spargrisen.Category;
+
 import java.util.List;
+
 
 public interface SpargrisenRepository {
 
-    public void deposit (double income, int id);
-  //  public double getBalance (double income);
+    void deposit(double income, int id);
+    List<Category> getCategories(int userId);
+    List<Category> getAllCategories();
+
 }
