8091170bfecbc3e3711b0856072b37edd9768ae3
Katrin Eckert <katrin.82.eckert@gmail.com>
2018-08-23 19:55:48 +0200

    Added functionality for register page

--- src/main/java/se/spargrisen/spargrisen/Repository/SpargrisenRepository.java
+++ src/main/java/se/spargrisen/spargrisen/Repository/SpargrisenRepository.java
@@ -2,6 +2,7 @@ package se.spargrisen.spargrisen.Repository;
 
 import se.spargrisen.spargrisen.Account;
 import se.spargrisen.spargrisen.Category;
+import se.spargrisen.spargrisen.User;
 
 import java.util.List;
 
@@ -14,5 +15,7 @@ public interface SpargrisenRepository {
     Account getAccount(int userId);
     List<Transaction> getTransactions(int account_id);
     List<Budget> getBudgets(int user_id);
+    User registerNewUser(String username, String password, String name);
+    void registerNewAccount(int user_ID);
 
 }
