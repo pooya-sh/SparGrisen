9eefe419bcb12fc2d0600db144908817290f3ec3
Pooya Sh <pooya.sh@me.com>
2018-08-23 14:20:21 +0200

    Add checkUsernamePassword method to JDBCSpargrisenRepository

--- src/main/java/se/spargrisen/spargrisen/User.java
+++ src/main/java/se/spargrisen/spargrisen/User.java
@@ -1,47 +1,11 @@
 package se.spargrisen.spargrisen;
 
 public class User {
-    int id;
-    String username;
+    int user_ID;
     String name;
-    String password;
 
-    public User(int id, String username, String name, String password) {
-        this.id = id;
-        this.username = username;
+    public User(int id, String name) {
+        this.user_ID = id;
         this.name = name;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
